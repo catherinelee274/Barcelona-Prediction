02bc495a55d47ca2299dc05f48374375aad2e4cd
catherinelee274 <catherinelee274@yahoo.com>
2019-06-06 11:53:40 -0700

    final (ish)

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
-
+#import pandas as pd
 blueprint = Blueprint('home', __name__)
 
 @blueprint.route('/')
 def index():
+    #df = pd.read_csv("count_datetimeindex.csv")
     return render_template('home/index2.html')
